0d0ad22b1ce7a57e84ceafa2dd3941d1b289535c
Ura <andreewura@gmail.com>
2024-05-22 20:27:08 +0200

    2024-05-02 1

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import threading
+import multiprocessing
+import time
+from datetime import datetime
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def most_usefull_function():
+    print('Function is started!')
+    time.sleep(5)
+    print('Function is ended!')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+start_time = datetime.now()
+# most_usefull_function()
+# most_usefull_function()
+# print('delay:', datetime.now() - start_time)
+
+
+my_thread_1 = threading.Thread(target=most_usefull_function)
+my_thread_2 = threading.Thread(target=most_usefull_function)
+my_thread_1.start()     #старт певрого потока
+my_thread_2.start()     #старт второго потока
+
+my_thread_1.join()  #главный поток должен ждать заверешиня первого потока
+my_thread_2.join()  #главный поток должен ждать заверешиня второго потока
+print('delay:', datetime.now() - start_time)
